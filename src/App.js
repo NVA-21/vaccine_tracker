5239507ce7c47097a1c70a005c8b48ebd12e81b8
Vishakh <vishakhg98@gmail.com>
2021-05-12 07:48:53 +0530

    Cleanups

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { fetchApiData, PUBLIC_IMAGE_PATH } from "./utils/Constants";
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [pinCode, setPinCode] = useState("");
 
-  // useEffect(() => {
-  //   console.log("useEffect Called");
-  //   searchVaccineSlots("110031");
-  //   // sendNotification();
-  // }, [pinCode]);
-
   async function searchVaccineSlots(searchQuery) {
     const responseValue = await fetchApiData(
       `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${searchQuery}&date=13-05-2021`
     );
-    console.log(responseValue.sessions);
+
     // Sending Push notification when vaccine center exists
     if (responseValue.sessions.length) {
       sendNotification();
@@ -24,11 +18,9 @@ function App() {
   }
 
   function sendNotification() {
-    console.log("Notification sent");
     alert("Notifcation");
   }
 
-  console.log(searchQuery);
   return (
     <div className="App">
       <input
